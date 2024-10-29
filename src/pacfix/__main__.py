6c354293be6cd94ee4933f9b1376b5deb2effcbf
Nguyễn Gia Phong <cnx@loang.net>
2024-10-29 14:40:00 +0900

    Expose API

--- src/pacfix/__main__.py
+++ src/pacfix/__main__.py
@@ -4,96 +4,65 @@ import argparse
 from contextlib import closing
 from functools import partial
 
-from . import __version__
-from . import invariant
-from . import synthesis
-from . import utils
+from . import __version__, Result, learn, utils
 
 
 def run(args: argparse.Namespace):
     input_dir = args.input_dir
-    pac_delta = args.pac_delta
-    output = args.output
-    # create initial hypothesis space
     with closing(args.live_vars):
         live_vars = utils.get_live_vars(args.live_vars)
-    synthesizer = synthesis.Synthesizer(live_vars)
-    hypothesis_space = synthesizer.synthesize()
-    original_size = len(hypothesis_space)
-    # validate hypothesis space
-    neg_vals_raw = utils.get_valuations(os.path.join(input_dir, "neg"))
-    pos_vals_raw = utils.get_valuations(os.path.join(input_dir, "pos"))
-    neg_vals_init, pos_vals_init = utils.parse_valuation(neg_vals_raw, pos_vals_raw)
-    initial_neg = len(neg_vals_init)
-    initial_pos = len(pos_vals_init)
-    neg_vals = utils.filter_duplicate(neg_vals_init)
-    pos_vals = utils.filter_duplicate(pos_vals_init)
-    refined_space = synthesizer.validate(hypothesis_space, neg_vals, pos_vals)
-    # write output
-    int_vars = 0
-    for v in live_vars.values():
-        if v.var_type == utils.VarType.INT:
-            int_vars += 1
+    vals_raw_neg = utils.get_valuations(os.path.join(args.input_dir, "neg"))
+    vals_raw_pos = utils.get_valuations(os.path.join(args.input_dir, "pos"))
+    vals_neg, vals_pos = utils.parse_valuation(vals_raw_neg, vals_raw_pos)
+    result = learn(live_vars, vals_neg, vals_pos, args.pac_delta)
+
+    output = args.output
+    int_vars = sum(v.var_type == utils.VarType.INT for v in live_vars.values())
     output.write(f"[metadata] [live-variables] [total {len(live_vars)}] [int {int_vars}]\n")
-    output.write(f"[metadata] [hypothesis-space] [original {original_size}] [final {len(refined_space)}]\n")
-    samples = len(neg_vals) + len(pos_vals)
-    non_uniq_samples = initial_neg + initial_pos
-    output.write(f"[metadata] [valuation] [neg {len(neg_vals)}] [pos {len(pos_vals)}] [uniq {samples}] [init-neg {initial_neg}] [init-pos {initial_pos}] [non-uniq {non_uniq_samples}]\n")
-    pac_epsilon = utils.calculate_pac(samples, len(hypothesis_space), pac_delta)
-    output.write(f"[metadata] [pac] [delta {pac_delta}] [eps {pac_epsilon}]\n")
-    pac_epsilon_no_uniq = utils.calculate_pac(non_uniq_samples, len(hypothesis_space), pac_delta)
-    output.write(f"[metadata] [pac-no-uniq] [delta {pac_delta}] [eps {pac_epsilon_no_uniq}]\n")
+    output.write("[metadata] [hypothesis-space]"
+        f" [original {result.size_orig}] [final {result.size_final}]\n")
+    output.write("[metadata] [valuation]"
+        f" [neg {result.samples_neg}] [pos {result.samples_pos}]"
+        f" [uniq {result.samples_neg + result.samples_pos}]"
+        f" [init-neg {len(vals_neg)}] [init-pos {len(vals_pos)}]"
+        f" [non-uniq {len(vals_neg) + len(vals_pos)}]\n")
+    output.write(f"[metadata] [pac] [delta {args.pac_delta}]"
+        f" [eps {result.pac_epsilon}]\n")
+    output.write(f"[metadata] [pac-no-uniq] [delta {args.pac_delta}]"
+        f" [eps {result.pac_epsilon_no_uniq}]\n")
     output.write("[final] --------------\n")
-    inv_manager = invariant.InvariantManager(live_vars)
-    inv_manager.reduce()
-    for inv in refined_space:
-        inv_manager.add_invariant(inv)
-    inv_manager.dump(output, args.output_smt)
+    result.inv_mgr.dump(output, args.output_smt)
 
 
 def run_uni(args: argparse.Namespace):
-    input_dir = args.input_dir
-    pac_delta = args.pac_delta
-    output = args.output
-    # create initial hypothesis space
     with closing(args.live_vars):
         live_vars = utils.get_live_vars(args.live_vars)
     if args.lv_file is not None:
         with closing(args.lv_file):
             used_lvs = utils.get_lv_file(args.lv_file)
         live_vars = {k: v for k, v in live_vars.items() if v.name in used_lvs}
-    synthesizer = synthesis.Synthesizer(live_vars)
-    hypothesis_space = synthesizer.synthesize()
-    original_size = len(hypothesis_space)
-    # validate hypothesis space
-    neg_vals_raw = utils.get_valuations(os.path.join(input_dir, "neg"))
-    pos_vals_raw = utils.get_valuations(os.path.join(input_dir, "pos"))
-    neg_vals_init, pos_vals_init = utils.parse_valuations_uni([], neg_vals_raw + pos_vals_raw)
-    initial_neg = len(neg_vals_init)
-    initial_pos = len(pos_vals_init)
-    neg_vals = utils.filter_duplicate(neg_vals_init)
-    pos_vals = utils.filter_duplicate(pos_vals_init)
-    refined_space = synthesizer.validate(hypothesis_space, neg_vals, pos_vals)
-    # write output
-    int_vars = 0
-    for v in live_vars.values():
-        if v.var_type == utils.VarType.INT:
-            int_vars += 1
+    vals_raw_neg = utils.get_valuations(os.path.join(args.input_dir, "neg"))
+    vals_raw_pos = utils.get_valuations(os.path.join(args.input_dir, "pos"))
+    vals_neg, vals_pos = utils.parse_valuations_uni([],
+        vals_raw_neg + vals_raw_pos)
+    result = learn(live_vars, vals_neg, vals_pos, args.pac_delta)
+
+    output = args.output
+    int_vars = sum(v.var_type == utils.VarType.INT for v in live_vars.values())
     output.write(f"[metadata] [live-variables] [total {len(live_vars)}] [int {int_vars}]\n")
-    output.write(f"[metadata] [hypothesis-space] [original {original_size}] [final {len(refined_space)}]\n")
-    samples = len(neg_vals) + len(pos_vals)
-    non_uniq_samples = initial_neg + initial_pos
-    output.write(f"[metadata] [valuation] [neg {len(neg_vals)}] [pos {len(pos_vals)}] [uniq {samples}] [init-neg {initial_neg}] [init-pos {initial_pos}] [non-uniq {non_uniq_samples}]\n")
-    pac_epsilon = utils.calculate_pac(samples, len(refined_space), pac_delta)
-    output.write(f"[metadata] [pac] [delta {pac_delta}] [eps {pac_epsilon}]\n")
-    pac_epsilon_no_uniq = utils.calculate_pac(non_uniq_samples, len(refined_space), pac_delta)
-    output.write(f"[metadata] [pac-no-uniq] [delta {pac_delta}] [eps {pac_epsilon_no_uniq}]\n")
+    output.write("[metadata] [hypothesis-space]"
+        f" [original {result.size_orig}] [final {result.size_final}]\n")
+    output.write("[metadata] [valuation]"
+        f" [neg {result.samples_neg}] [pos {result.samples_pos}]"
+        f" [uniq {result.samples_neg + result.samples_pos}]"
+        f" [init-neg {len(vals_neg)}] [init-pos {len(vals_pos)}]"
+        f" [non-uniq {len(vals_neg) + len(vals_pos)}]\n")
+    output.write(f"[metadata] [pac] [delta {args.pac_delta}]"
+        f" [eps {result.pac_epsilon}]\n")
+    output.write(f"[metadata] [pac-no-uniq] [delta {args.pac_delta}]"
+        f" [eps {result.pac_epsilon_no_uniq}]\n")
     output.write("[final] --------------\n")
-    inv_manager = invariant.InvariantManager(live_vars)
-    inv_manager.reduce()
-    for inv in refined_space:
-        inv_manager.add_invariant(inv)
-    inv_manager.dump(output, "")
+    result.inv_mgr.dump(output, None)
 
 
 def directory(path: str, read: bool) -> str:
